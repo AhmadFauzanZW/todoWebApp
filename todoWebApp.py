ca7c7609c220ace7e51cbfc16e53b999a035ddd7
AhmadFauzanZW <ahfa201204@gmail.com>
2025-02-12 17:33:23 +0700

    add warning below program

--- todoWebApp.py
+++ todoWebApp.py
@@ -2,17 +2,18 @@ import streamlit as st
 import functions
 
 filepath = 'TodoLists.txt'
-
 todos = functions.imporData(filepath)
 
 def tambah_todo():
-    # Check if the input is not empty
     if st.session_state['todo']:
         todo = st.session_state['todo'] + '\n'
-        todos.append(todo)
-        functions.eksporData(filepath, todos)
-        # Clear the input field
-        st.session_state['todo'] = ''
+        # Check if todo already exists (ignoring newlines and spaces)
+        if todo.strip() in [existing.strip() for existing in todos]:
+            st.error("Todo ini sudah ada dalam daftar! Silakan masukkan todo yang berbeda.")
+        else:
+            todos.append(todo)
+            functions.eksporData(filepath, todos)
+            st.session_state['todo'] = ''
 
 st.title("Todo List Program")
 st.subheader("By Ahmad Fauzan")
@@ -20,16 +21,16 @@ st.divider()
 
 with st.container(border=True):
     for index, todo in enumerate(todos):
-        checkbox = st.checkbox(todo, key=todo)
+        # Use index as part of the key to ensure uniqueness
+        checkbox = st.checkbox(todo, key=f"todo_{index}")
         if checkbox:
             todos.pop(index)
             functions.eksporData(filepath, todos)
-            del st.session_state[todo]
+            # Use the same key format when deleting from session state
+            del st.session_state[f"todo_{index}"]
             st.rerun()
 
 st.divider()
 
 st.text_input(label='Masukkan Todo:', placeholder="Ex. Debugging Code", key='todo',
               on_change=tambah_todo)
-
-st.warning("Harap masukkan todo yang berbeda. Jangan masukkan todo yang sama!")
